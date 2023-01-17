71debc5b56b96663702cde0dce1752fb6db3c3d0
GesleyMarinho <gesleymarinho@gmail.com>
2023-01-17 20:41:37 -0300

    criação do tabuleiro e as posições de  8 a 1 e de A a H

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -2,11 +2,13 @@ package Aplication;
 
 import BoardGame.Board;
 import BoardGame.Position;
+import Chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Board board = new Board(8,8);
 
-        System.out.println(board);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
