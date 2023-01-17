63ae35beabddc9fc622f8f4609bac52211818daf
GesleyMarinho <gesleymarinho@gmail.com>
2023-01-17 20:12:21 -0300

    tabuleiro e peças criados

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,12 +1,12 @@
 package Aplication;
 
+import BoardGame.Board;
 import BoardGame.Position;
 
 public class Program {
     public static void main(String[] args) {
+        Board board = new Board(8,8);
 
-        Position position = new Position(3,5);
-
-        System.out.println(position);
+        System.out.println(board);
     }
 }
