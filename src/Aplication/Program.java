3dd7b3f8735d6f9c14714bc5c79e073f1ce53b88
GesleyMarinho <gesleymarinho@gmail.com>
2023-01-17 19:58:35 -0300

    linha e coluna criados

---
+++ src/Aplication/Program.java
@@ -0,0 +1,12 @@
+package Aplication;
+
+import BoardGame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position position = new Position(3,5);
+
+        System.out.println(position);
+    }
+}
